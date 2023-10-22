5c20903c1863e8dee64c8317b256ce54c7354f2e
Thomas K <kulik.tamas@gmail.com>
2023-10-22 21:26:21 +0200

    Win10: update notification popup

--- Windows/disable-update-notification.bat
+++ Windows/disable-update-notification.bat
@@ -1,26 +1,9 @@
 @echo off
 
-:: Source (based on): https://winaero.com/disable-updates-available-windows-10/
-:: They really should stop annoying you with unnecessary messages
+:: Solution's source: https://superuser.com/questions/1214503/disable-windows-10-update-notification-osd/1431277#1431277
+:: This redirects the Windows Update notification calls to the system tray, so it doesn't actually get displayed
+:: This should be the preferred method, rather than denying the file permissions, because then updates will not affect this, and there is no need for SFC to fix those files either (which may happen)
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MusNotifyIcon.exe" /v "Debugger" /t REG_SZ /d "%windir%\System32\systray.exe" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MusNotification.exe" /v "Debugger" /t REG_SZ /d "%windir%\System32\systray.exe" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MusNotificationUx.exe" /v "Debugger" /t REG_SZ /d "%windir%\System32\systray.exe" /f >nul 2>&1
 
-:: This has to be used again after Windows updates!
-:: S-1-1-0 (SID) = Everyone (this makes it Windows-language independent)
-
-cd /d "%Windir%\System32"
-if exist "MusNotification.exe" (
-    takeown /f MusNotification.exe
-    icacls MusNotification.exe /remove:d *S-1-1-0
-    icacls MusNotification.exe /deny *S-1-1-0:^(X^)
-)
-
-if exist "MusNotificationUx.exe" (
-    takeown /f MusNotificationUx.exe
-    icacls MusNotificationUx.exe /remove:d *S-1-1-0
-    icacls MusNotificationUx.exe /deny *S-1-1-0:^(X^)
-)
-
-if exist "MusNotifyIcon.exe" (
-    takeown /f MusNotifyIcon.exe
-    icacls MusNotifyIcon.exe /remove:d *S-1-1-0
-    icacls MusNotifyIcon.exe /deny *S-1-1-0:^(X^)
-)
--- Windows/enable-update-notification.bat
+++ Windows/enable-update-notification.bat
@@ -1,18 +1,6 @@
 @echo off
 
-:: Source: https://winaero.com/disable-updates-available-windows-10/
-:: S-1-1-0 (SID) = Everyone (this makes it language independent)
+reg delete "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MusNotifyIcon.exe" /v "Debugger" /f >nul 2>&1
+reg delete "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MusNotification.exe" /v "Debugger" /f >nul 2>&1
+reg delete "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\MusNotificationUx.exe" /v "Debugger" /f >nul 2>&1
 
-cd /d "%Windir%\System32"
-icacls MusNotification.exe /remove:d *S-1-1-0
-icacls MusNotification.exe /grant *S-1-1-0:F
-icacls MusNotification.exe /setowner "NT SERVICE\TrustedInstaller"
-icacls MusNotification.exe /remove:g *S-1-1-0
-icacls MusNotificationUx.exe /remove:d *S-1-1-0
-icacls MusNotificationUx.exe /grant *S-1-1-0:F
-icacls MusNotificationUx.exe /setowner "NT SERVICE\TrustedInstaller"
-icacls MusNotificationUx.exe /remove:g *S-1-1-0
-icacls MusNotifyIcon.exe /remove:d *S-1-1-0
-icacls MusNotifyIcon.exe /grant *S-1-1-0:F
-icacls MusNotifyIcon.exe /setowner "NT SERVICE\TrustedInstaller"
-icacls MusNotifyIcon.exe /remove:g *S-1-1-0
